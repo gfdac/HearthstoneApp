dbd68fe30c142f0bf11b02847da6cba922c41b86
Gustavo Clemente <gfdac@live.com>
2023-05-27 20:01:12 -0300

    #4: implementation of the CardListPresenter class

---
+++ HearthstoneApp/Modules/CardList/CardListPresenter.swift
@@ -0,0 +1,36 @@
+//
+//  CardListPresenter.swift
+//  HearthstoneApp
+//
+//  Created by Guh F on 27/05/23.
+//
+
+import Foundation
+
+protocol CardListPresenterProtocol {
+    func fetchCards()
+    func presentCards(_ cards: [CardListModels.Card])
+    func presentError(message: String)
+}
+
+class CardListPresenter: CardListPresenterProtocol {
+    weak var view: CardListViewProtocol?
+    var interactor: CardListInteractorProtocol
+    
+    init(interactor: CardListInteractorProtocol) {
+        self.interactor = interactor
+    }
+    
+    func fetchCards() {
+        interactor.fetchCards()
+    }
+    
+    func presentCards(_ cards: [CardListModels.Card]) {
+        view?.displayCards(cards)
+    }
+    
+    func presentError(message: String) {
+        view?.displayError(message: message)
+    }
+}
+
