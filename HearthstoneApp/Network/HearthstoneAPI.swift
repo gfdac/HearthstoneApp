4644ba675c998a40094f312835ccc6e5c0400e88
Gustavo Clemente <gfdac@live.com>
2023-05-27 20:58:17 -0300

    #2: HearthstoneAPIService getCardDetail

--- HearthstoneApp/Network/HearthstoneAPI.swift
+++ HearthstoneApp/Network/HearthstoneAPI.swift
@@ -8,33 +8,72 @@
 import Foundation
 
 protocol HearthstoneAPIProtocol {
-    func getAllCards(completion: @escaping (Result<[String: [CardListModels.Card]], Error>) -> Void)
+    func getAllCards(completion: @escaping (Result<[CardListModels.Card], Error>) -> Void)
+    func getCardDetail(cardId: String, completion: @escaping (Result<CardDetailModels.CardDetail, Error>) -> Void)
 }
 
-class HearthstoneAPI: HearthstoneAPIProtocol {
-    private let networkService: NetworkService
-    
-    init(networkService: NetworkService) {
-        self.networkService = networkService
+class HearthstoneAPIService: HearthstoneAPIProtocol {
+    private let baseURL = "https://omgvamp-hearthstone-v1.p.rapidapi.com"
+
+    private func buildRequest(urlPath: String) -> URLRequest {
+        let url = URL(string: baseURL + urlPath)!
+        var request = URLRequest(url: url)
+        request.addValue("535b35baeamsh714f8ecc16c828cp1c90c0jsn0d8104b3289f", forHTTPHeaderField: "X-RapidAPI-Key")
+        request.addValue("omgvamp-hearthstone-v1.p.rapidapi.com", forHTTPHeaderField: "X-RapidAPI-Host")
+        return request
     }
-    
-    func getAllCards(completion: @escaping (Result<[String: [CardListModels.Card]], Error>) -> Void) {
-        let endpoint = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards"
-        
-        networkService.request(endpoint: endpoint) { result in
-            switch result {
-            case .success(let data):
-                do {
-                    let response = try JSONDecoder().decode([String: [CardListModels.Card]].self, from: data)
-                    completion(.success(response))
-                } catch {
-                    completion(.failure(error))
-                }
-            case .failure(let error):
+
+    func getAllCards(completion: @escaping (Result<[CardListModels.Card], Error>) -> Void) {
+        let urlPath = "/cards"
+        let request = buildRequest(urlPath: urlPath)
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(HearthstoneAPIError.invalidData))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let cards = try decoder.decode([CardListModels.Card].self, from: data)
+                completion(.success(cards))
+            } catch {
                 completion(.failure(error))
             }
-        }
+        }.resume()
+    }
+
+    func getCardDetail(cardId: String, completion: @escaping (Result<CardDetailModels.CardDetail, Error>) -> Void) {
+        let urlPath = "/cards/\(cardId)"
+        let request = buildRequest(urlPath: urlPath)
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(HearthstoneAPIError.invalidData))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let cardDetail = try decoder.decode(CardDetailModels.CardDetail.self, from: data)
+                completion(.success(cardDetail))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
-
+enum HearthstoneAPIError: Error {
+    case invalidData
+}
