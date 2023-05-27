b48d1cf5a2fad69a05e414e40437db8c55777a92
Gustavo Clemente <gfdac@live.com>
2023-05-27 17:56:16 -0300

    Initial Commit

---
+++ HearthstoneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HearthstoneApp
+//
+//  Created by Guh F on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
