3a6a4217435986741f6b5d6e16299716f9d9a28e
Gustavo Clemente <gfdac@live.com>
2023-05-27 18:27:42 -0300

    changing flow to view code in appdelegate
    "#1: changing flow to view code in appdelegate"

--- HearthstoneApp/AppDelegate.swift
+++ HearthstoneApp/AppDelegate.swift
@@ -11,11 +11,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let cardListViewController = CardListViewController()
+        
+        let navigationController = UINavigationController(rootViewController: cardListViewController)
+
+        // Set CardListViewController as the rootViewController
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
+
 
     // MARK: UISceneSession Lifecycle
 
