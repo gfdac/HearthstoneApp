993592d179dc6cac1d80a97e607021f661c9ed08
Gustavo Clemente <gfdac@live.com>
2023-05-27 20:57:17 -0300

    5: implementation of the CardDetailInteractor class

---
+++ HearthstoneApp/Modules/CardDetail/CardDetailInteractor.swift
@@ -0,0 +1,36 @@
+//
+//  CardDetailInteractor.swift
+//  HearthstoneApp
+//
+//  Created by Guh F on 27/05/23.
+//
+
+import Foundation
+
+protocol CardDetailInteractorProtocol {
+    func fetchCardDetail()
+}
+
+class CardDetailInteractor: CardDetailInteractorProtocol {
+    weak var presenter: CardDetailPresenterProtocol?
+    let apiService: HearthstoneAPIProtocol
+    let card: CardListModels.Card
+    
+    init(apiService: HearthstoneAPIProtocol, card: CardListModels.Card) {
+        self.apiService = apiService
+        self.card = card
+    }
+    
+    func fetchCardDetail() {
+        // Use a API do Hearthstone para buscar os detalhes do cartão usando o cardId
+        apiService.getCardDetail(cardId: card.cardId) { [weak self] result in
+            switch result {
+            case .success(let cardDetail):
+                self?.presenter?.presentCardDetail(cardDetail)
+            case .failure(let error):
+                self?.presenter?.presentError(message: error.localizedDescription)
+            }
+        }
+    }
+}
+
